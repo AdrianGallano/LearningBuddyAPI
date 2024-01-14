91e8a20ce7e8d6cc12d841dbc75ce3d517547265
Adrian Gallano <adriangallanoengr@gmail.com>
2024-01-14 15:27:45 +0800

    Add flashcards models

--- learning_buddy/flashcards/models.py
+++ learning_buddy/flashcards/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from accounts.models import User
+import uuid
 
-# Create your models here.
+class Room(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Subject(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name = models.CharField(max_length=100)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+
+class Category(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name = models.CharField(max_length=50)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+
+class Topic(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name = models.CharField(max_length=100)
+    date_time_created = models.DateTimeField(auto_now_add=True)
+    date_time_modified = models.DateTimeField(auto_now=True)
+    score = models.IntegerField()
+    number_of_items = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Item(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    question = models.TextField()
+    answer = models.TextField()
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
