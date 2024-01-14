5272dc1546ded2ff3bd71126a8008a242dbc773a
Adrian Gallano <adriangallanoengr@gmail.com>
2024-01-14 15:09:28 +0800

    Rename review models

--- learning_buddy/reviews/models.py
+++ learning_buddy/reviews/models.py
@@ -2,33 +2,33 @@ from django.db import models
 from accounts.models import User
 
 
-class ReviewRoom(models.Model):
+class Room(models.Model):
     id = models.UUIDField(primary_key=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
-class ReviewSubject(models.Model):
+class Subject(models.Model):
     id = models.UUIDField(primary_key=True)
     name = models.CharField(max_length=100)
-    room = models.ForeignKey(ReviewRoom, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
 
 
-class ReviewCategory(models.Model):
+class Category(models.Model):
     id = models.UUIDField(primary_key=True)
     name = models.CharField(max_length=50)
-    subject = models.ForeignKey(ReviewSubject, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
 
 
-class ReviewTopic(models.Model):
+class Topic(models.Model):
     id = models.UUIDField(primary_key=True)
     name = models.CharField(max_length=100)
     date_time_created = models.DateTimeField(auto_now_add=True)
     date_time_modified = models.DateTimeField(auto_now=True)
-    category = models.ForeignKey(ReviewCategory, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
-class ReviewItem(models.Model):
+class Item(models.Model):
     id = models.UUIDField(primary_key=True)
     question = models.TextField()
     answer = models.TextField()
-    topic = models.ForeignKey(ReviewTopic, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
