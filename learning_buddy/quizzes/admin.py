46b7ff64b91c2e766cc52e337663c6d8c30be017
Adrian Gallano <adriangallanoengr@gmail.com>
2024-01-15 23:11:09 +0800

    Add quizzes models to admin

--- learning_buddy/quizzes/admin.py
+++ learning_buddy/quizzes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Room, Subject, Category, Topic, Item, FakeAnswer
 
-# Register your models here.
+admin.site.register(Room)
+admin.site.register(Subject)
+admin.site.register(Category)
+admin.site.register(Topic)
+admin.site.register(Item)
+admin.site.register(FakeAnswer)
