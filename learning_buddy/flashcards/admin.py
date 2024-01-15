465409b5bfe2b8f7f1de773ada16f33e723c4790
Adrian Gallano <adriangallanoengr@gmail.com>
2024-01-15 23:10:46 +0800

    Add flashcards models to admin

--- learning_buddy/flashcards/admin.py
+++ learning_buddy/flashcards/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Room, Subject, Category, Topic, Item
 
-# Register your models here.
+
+admin.site.register(Room)
+admin.site.register(Subject)
+admin.site.register(Category)
+admin.site.register(Topic)
+admin.site.register(Item)
