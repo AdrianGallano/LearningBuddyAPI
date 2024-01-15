9f864851aa71f0c0505f0d863fcd38a617a2be34
Adrian Gallano <adriangallanoengr@gmail.com>
2024-01-15 23:10:28 +0800

    Add str representation for user

--- learning_buddy/accounts/models.py
+++ learning_buddy/accounts/models.py
@@ -7,3 +7,6 @@ class User(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=100)
     password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
