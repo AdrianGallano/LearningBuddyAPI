d64af7b2e8afaf6fc2a431b3fb605a26e86d99d6
Adrian Gallano <adriangallanoengr@gmail.com>
2024-01-18 23:49:14 +0800

    Update admin.py better customization

--- learning_buddy/accounts/admin.py
+++ learning_buddy/accounts/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
-from accounts.models import User
+from django.contrib.auth.models import User as AuthUser
+from .models import User
+from django.contrib.auth.admin import UserAdmin
 
-class UserAdmin(admin.ModelAdmin):
+admin.site.unregister(AuthUser)
+
+
+@admin.register(User)
+class UserAccountsAdmin(admin.ModelAdmin):
     list_display = ["first_name", "last_name", "email"]
+    search_fields = ["email__startswith"]
 
+@admin.register(AuthUser)
+class NewUser(UserAdmin):
+    readonly_fields = [
+        "date_joined",
+    ]
+    
+    # disable staff privelleges to edit username
+    def get_form(self, request, object=None, **kwargs):
+        form = super().get_form(request, object, **kwargs)
+        is_superuser = request.user.is_superuser
 
-admin.site.register(User, UserAdmin)
+        if not is_superuser:
+            form.base_fields["username"].disabled = True
+            form.base_fields["email"].disabled = True
+            form.base_fields["user_permissions"].disabled = True
+        
+        return form
