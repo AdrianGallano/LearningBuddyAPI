48c32f36277cdbfa0bb44ef934840d34242fb1a6
Adrian Gallano <adriangallanoengr@gmail.com>
2024-01-20 22:11:58 +0800

    Change database configuration to mysql

--- learning_buddy/learning_buddy/settings.py
+++ learning_buddy/learning_buddy/settings.py
@@ -82,8 +82,15 @@ WSGI_APPLICATION = 'learning_buddy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USERNAME"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
+        'OPTIONS': {
+            "init_command":"SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
