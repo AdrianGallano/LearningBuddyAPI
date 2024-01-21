06f05d5bf0d6f533bf41bf50ca7316b8183dc88a
Adrian Gallano <adriangallanoengr@gmail.com>
2024-01-21 16:09:51 +0800

    Add confirm password

--- learning_buddy/accounts/forms.py
+++ learning_buddy/accounts/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import User
 
 class UserForm(forms.ModelForm):
+    confirm_password = forms.CharField(widget=forms.PasswordInput(), min_length=8, max_length=128)
     class Meta:
         model = User
         fields = ["username", "first_name", "last_name", "email", "password"]
@@ -11,6 +12,7 @@ class UserForm(forms.ModelForm):
 
 
 class LoginForm(forms.ModelForm):
+    confirm_password = forms.CharField(widget=forms.PasswordInput(), min_length=8, max_length=128)
     class Meta:
         model = User
         fields = ["username", "password"]
