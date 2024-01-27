8fdf3c8693757b176ffa077a9d5a7270232c4cb0
Adrian Gallano <adriangallanoengr@gmail.com>
2024-01-27 13:00:19 +0800

    Update forms and models

--- learning_buddy/accounts/forms.py
+++ learning_buddy/accounts/forms.py
@@ -1,21 +1,37 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate
 from .models import User
 
-class UserForm(forms.ModelForm):
-    confirm_password = forms.CharField(widget=forms.PasswordInput(), min_length=8, max_length=128)
+
+class RegisterForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ["username", "first_name", "last_name", "email", "password"]
-        widgets = {
-            "password": forms.PasswordInput(attrs={"minlength":8}),
-        }
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "password1",
+            "password2",
+        ]
 
 
 class LoginForm(forms.ModelForm):
-    confirm_password = forms.CharField(widget=forms.PasswordInput(), min_length=8, max_length=128)
+    def clean(self):
+        username = self.cleaned_data["username"]
+        password = self.cleaned_data["password"]
+        user = authenticate(username=username, password=password)
+
+        if not user or not user.is_active:
+            msg = "username or password does not exist"
+            self.add_error("password", msg)
+        return self.cleaned_data
+
     class Meta:
         model = User
         fields = ["username", "password"]
         widgets = {
-            "password": forms.PasswordInput(attrs={"minlength":8}),
+            "username": forms.TextInput(attrs={"class":"login_field", "placeholder":"Username"}),
+            "password": forms.PasswordInput(attrs={"minlength":"8", "class":"login_field", "placeholder":"Password" })
         }
--- learning_buddy/accounts/models.py
+++ learning_buddy/accounts/models.py
@@ -1,11 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    username = models.CharField(max_length=32, unique=True)
-
-    USERNAME_FIELD = "username"
-
     def __str__(self):
         return f"{str(self.pk)} - {self.username}"
