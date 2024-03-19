262af3d04a7d9e0b8ac690562d406ee123d4b4bb
Adrian Gallano <adriangallanoengr@gmail.com>
2024-03-19 23:41:27 +0800

    Add create, and delete subjects in reviews

--- learning_buddy/reviews/urls.py
+++ learning_buddy/reviews/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
-from .views import ReviewsSubjectsListView
+from .views import ReviewsSubjectsListView, ReviewsSubjectsDeleteView, ReviewsSubjectCreateView
 app_name = "reviews"
 
 urlpatterns = [
-    path("", ReviewsSubjectsListView.as_view(), name="display_subject"),
+    path("subjects/", ReviewsSubjectsListView.as_view(), name="display_subjects"),
+    path("create_subject/", ReviewsSubjectCreateView.as_view(), name="create_subject"),
+    path("delete_subject/<uuid:subject_id>", ReviewsSubjectsDeleteView.as_view(), name="delete_subject"),
 ]
--- learning_buddy/reviews/views.py
+++ learning_buddy/reviews/views.py
@@ -1,7 +1,8 @@
 from typing import Any
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import ListView, DeleteView
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponsePermanentRedirect
+from django.views.generic import ListView, DeleteView, View
 from .models import Room, Subject
 
 
@@ -14,7 +15,7 @@ def create_room(user):
 
 
 class ReviewsSubjectsListView(ListView):
-    template_name = "reviews/display_subjects.html"
+    template_name = "reviews/subjects/display_subjects.html"
     model = Subject
     context_object_name = "subjects"
     
@@ -24,5 +25,27 @@ class ReviewsSubjectsListView(ListView):
         context["user"] = self.request.user 
         return context
 
-class ReviewSubjectsDeleteView(DeleteView):
+class ReviewsSubjectsDeleteView(View):
+    def get(self, request, subject_id):
+        subject = Subject.objects.get(id=subject_id)
+        return render(request, "reviews/subjects/delete_subject.html", {"subject":subject})
+
+    def post(self, request, subject_id):
+        Subject.objects.filter(id=subject_id).delete()
+        return HttpResponsePermanentRedirect(redirect_to=reverse("reviews:display_subjects"))
+    
+class ReviewsSubjectCreateView(View):
+    def get(self, request):
+        user = request.user
+        return render(request, "reviews/subjects/create_subject.html", {"user":user})
+
+    def post(self, request):
+        name=request.POST["name"]
+        room_id=request.POST["reviews_room_id"]
+        room = Room.objects.get(id=room_id)
+        Subject.objects.create(name=name, room=room)
+        return HttpResponsePermanentRedirect(redirect_to=reverse("reviews:display_subjects"))
+
+
+class ReviewsSubjectsUpdateView(View):
     pass
