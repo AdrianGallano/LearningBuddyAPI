d9a723fdc98a794fd2ba0df6130d8e010e129910
Adrian Gallano <adriangallanoengr@gmail.com>
2024-03-21 19:24:05 +0800

    Add category and topic

--- learning_buddy/reviews/urls.py
+++ learning_buddy/reviews/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 from . import views
-from .views import ReviewsSubjectsListView, ReviewsSubjectsDeleteView, ReviewsSubjectCreateView, ReviewsSubjectEditView
+from .views import SubjectListView, SubjectDeleteView, SubjectCreateView, SubjectEditView, CategoryListView, CategoryCreateView, CategoryDeleteView, CategoryEditView, TopicListView, TopicCreateView, TopicDeleteView, TopicEditView
 app_name = "reviews"
-
 urlpatterns = [
-    path("subjects/", ReviewsSubjectsListView.as_view(), name="display_subjects"),
-    path("create_subject/", ReviewsSubjectCreateView.as_view(), name="create_subject"),
-    path("edit_subject/<uuid:subject_id>", ReviewsSubjectEditView.as_view(), name="edit_subject"),
-    path("delete_subject/<uuid:subject_id>", ReviewsSubjectsDeleteView.as_view(), name="delete_subject"),
+    path("subject/", SubjectListView.as_view(), name="display_subjects"),
+    path("subject/create/", SubjectCreateView.as_view(), name="create_subject"),
+    path("subject/edit/<uuid:subject_id>/", SubjectEditView.as_view(), name="edit_subject"),
+    path("subject/delete/<uuid:subject_id>/", SubjectDeleteView.as_view(), name="delete_subject"),
+    path("subject/<uuid:subject_id>/category/", CategoryListView.as_view(), name="display_categorys"),
+    path("subject/<uuid:subject_id>/category/create/", CategoryCreateView.as_view(), name="create_category"),
+    path("subject/<uuid:subject_id>/category/edit/<uuid:category_id>/", CategoryEditView.as_view(), name="edit_category"),
+    path("subject/<uuid:subject_id>/category/delete/<uuid:category_id>/", CategoryDeleteView.as_view(), name="delete_category"),
+    path("subject/<uuid:subject_id>/category/<uuid:category_id>/topic", TopicListView.as_view(), name="display_topics"),
+    path("subject/<uuid:subject_id>/category/<uuid:category_id>/topic/create/", TopicCreateView.as_view(), name="create_topic"),
+    path("subject/<uuid:subject_id>/category/<uuid:category_id>/topic/edit/<uuid:topic_id>/", TopicEditView.as_view(), name="edit_topic"),
+    path("subject/<uuid:subject_id>/category/<uuid:category_id>/topic/delete/<uuid:topic_id>/", TopicDeleteView.as_view(), name="delete_topic"),
 ]
--- learning_buddy/reviews/views.py
+++ learning_buddy/reviews/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponsePermanentRedirect
 from django.views.generic import ListView, DeleteView, View
-from .models import Room, Subject
+from .models import Room, Subject, Category, Topic, Item
 
 
 def create_room(user):
@@ -15,7 +15,7 @@ def create_room(user):
     return False
 
 
-class ReviewsSubjectsListView(ListView):
+class SubjectListView(ListView):
     template_name = "reviews/subjects/display_subjects.html"
     model = Subject
     context_object_name = "subjects"
@@ -31,7 +31,7 @@ class ReviewsSubjectsListView(ListView):
         context["user"] = self.request.user 
         return context
 
-class ReviewsSubjectsDeleteView(View):
+class SubjectDeleteView(View):
     def get(self, request, subject_id):
         subject = Subject.objects.get(id=subject_id)
         return render(request, "reviews/subjects/delete_subject.html", {"subject":subject})
@@ -40,7 +40,7 @@ class ReviewsSubjectsDeleteView(View):
         Subject.objects.filter(id=subject_id).delete()
         return HttpResponsePermanentRedirect(redirect_to=reverse("reviews:display_subjects"))
     
-class ReviewsSubjectCreateView(View):
+class SubjectCreateView(View):
     def get(self, request):
         user = request.user
         return render(request, "reviews/subjects/create_subject.html", {"user":user})
@@ -53,7 +53,7 @@ class ReviewsSubjectCreateView(View):
         return HttpResponsePermanentRedirect(redirect_to=reverse("reviews:display_subjects"))
 
 
-class ReviewsSubjectEditView(View):
+class SubjectEditView(View):
     def get(self, request, subject_id):
         subject = Subject.objects.get(id=subject_id)
         return render(request, "reviews/subjects/edit_subject.html", {"subject":subject})
@@ -63,3 +63,128 @@ class ReviewsSubjectEditView(View):
         subject.name = request.POST["name"]
         subject.save()
         return HttpResponsePermanentRedirect(redirect_to=reverse("reviews:display_subjects"))
+
+
+class CategoryListView(ListView):
+    template_name = "reviews/categorys/display_categorys.html"
+    model = Category
+    context_object_name = "categorys"
+    paginate_by = 10
+    
+    
+    def get_queryset(self):
+        subject_id= self.kwargs["subject_id"]
+        subject = Subject.objects.get(id=subject_id)
+        return Category.objects.filter(subject=subject).order_by("-date_time_modified")
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        subject_id= self.kwargs["subject_id"]
+        subject = Subject.objects.get(id=subject_id)
+
+        context["app_name"] = "Categories" 
+        context["subject"] = subject
+        context["user"] = self.request.user 
+        return context
+
+class CategoryCreateView(View):
+    def get(self, request, subject_id):
+        subject = Subject.objects.get(id=subject_id)
+        return render(request, "reviews/categorys/create_category.html", {"subject":subject})
+
+    def post(self, request, subject_id):
+        name=request.POST["name"]
+
+        subject = Subject.objects.get(id=subject_id)
+        Category.objects.create(name=name, subject=subject)
+        return HttpResponsePermanentRedirect(redirect_to=reverse("reviews:display_categorys", kwargs={"subject_id": subject_id}))
+
+class CategoryDeleteView(View):
+    def get(self, request, subject_id, category_id):
+        category = Category.objects.get(id=category_id)
+        subject = Subject.objects.get(id=subject_id)
+
+        return render(request, "reviews/categorys/delete_category.html", {"category":category, "subject": subject})
+
+    def post(self, request, subject_id, category_id):
+        Category.objects.filter(id=category_id).delete()
+
+        return HttpResponsePermanentRedirect(redirect_to=reverse("reviews:display_categorys", kwargs={"subject_id":subject_id}))
+
+class CategoryEditView(View):
+    def get(self, request, subject_id, category_id):
+        category = Category.objects.get(id=category_id)
+        subject = Subject.objects.get(id=subject_id)
+
+        return render(request, "reviews/categorys/edit_category.html", {"category":category, "subject": subject})
+
+    def post(self, request, subject_id, category_id):
+        category = Category.objects.get(id=category_id)
+        category.name = request.POST["name"]
+        category.save()
+        return HttpResponsePermanentRedirect(redirect_to=reverse("reviews:display_categorys", kwargs={"subject_id":subject_id}))
+
+
+class TopicListView(ListView):
+    template_name = "reviews/topics/display_topics.html"
+    model = Topic
+    context_object_name = "topics"
+    paginate_by = 10
+    
+    
+    def get_queryset(self):
+        category_id= self.kwargs["category_id"]
+        category = Category.objects.get(id=category_id)
+        return Topic.objects.filter(category=category).order_by("-date_time_modified")
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        subject_id= self.kwargs["subject_id"]
+        subject = Subject.objects.get(id=subject_id)
+        category_id= self.kwargs["category_id"]
+        category = Category.objects.get(id=category_id)
+
+        context["app_name"] = "Topics" 
+        context["subject"] = subject
+        context["category"] = category
+        context["user"] = self.request.user 
+        return context
+
+class TopicCreateView(View):
+    def get(self, request, subject_id, category_id):
+        subject = Subject.objects.get(id=subject_id)
+        category = Category.objects.get(id=category_id)
+        return render(request, "reviews/topics/create_topic.html", {"subject":subject, "category": category})
+
+    def post(self, request, subject_id, category_id):
+        name=request.POST["name"]
+
+        category = Category.objects.get(id=category_id)
+        Topic.objects.create(name=name, category=category)
+        return HttpResponsePermanentRedirect(redirect_to=reverse("reviews:display_topics", kwargs={"subject_id": subject_id, "category_id": category_id}))
+
+class TopicDeleteView(View):
+    def get(self, request, subject_id, category_id, topic_id):
+        category = Category.objects.get(id=category_id)
+        subject = Subject.objects.get(id=subject_id)
+        topic = Topic.objects.get(id=topic_id)
+        return render(request, "reviews/topics/delete_topic.html", {"category":category, "subject": subject, "topic": topic})
+
+    def post(self, request, subject_id, category_id, topic_id):
+        Topic.objects.filter(id=topic_id).delete()
+
+        return HttpResponsePermanentRedirect(redirect_to=reverse("reviews:display_topics", kwargs={"subject_id":subject_id, "category_id": category_id}))
+
+class TopicEditView(View):
+    def get(self, request, subject_id, category_id, topic_id):
+        category = Category.objects.get(id=category_id)
+        subject = Subject.objects.get(id=subject_id)
+        topic = Topic.objects.get(id=topic_id)
+
+        return render(request, "reviews/topics/edit_topic.html", {"category":category, "subject": subject, "topic":topic})
+
+    def post(self, request, subject_id, category_id, topic_id):
+        topic = Topic.objects.get(id=topic_id)
+        topic.name = request.POST["name"]
+        topic.save()
+        return HttpResponsePermanentRedirect(redirect_to=reverse("reviews:display_topics", kwargs={"subject_id":subject_id, "category_id": category_id}))
