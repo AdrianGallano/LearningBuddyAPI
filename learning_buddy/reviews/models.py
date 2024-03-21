8622e6eb995ce0ed4b8393531ceac79911e3956d
Adrian Gallano <adriangallanoengr@gmail.com>
2024-03-21 19:23:07 +0800

    Add category date and time labels

--- learning_buddy/reviews/models.py
+++ learning_buddy/reviews/models.py
@@ -28,6 +28,8 @@ class Category(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     name = models.CharField(max_length=50)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    date_time_created = models.DateTimeField(auto_now_add=True)
+    date_time_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
